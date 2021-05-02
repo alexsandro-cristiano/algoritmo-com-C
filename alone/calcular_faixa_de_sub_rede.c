1572d86bb755a5dc288b1fd7b9e4d3ad1d22552e
Alexsandro Cristiano <75186371+alexsandro-cristiano@users.noreply.github.com>
2021-05-02 19:05:45 -0300

    versao 1.1 de apresentar faixa de subrede

--- alone/calcular_faixa_de_sub_rede.c
+++ alone/calcular_faixa_de_sub_rede.c
@@ -1,19 +1,23 @@
 #include <stdio.h>
 
- // 20                   255.255.255.240  1048576            14           /28
-  
 int main () {
 	
-	int n=1048576;
+	int qtda_subRede=32;
+	int host=8;
 	int i,j;
 	
+	printf("Digite a quantidade de sub redes: ");
+	scanf("%d",&qtda_subRede);
+	printf("Digite a quantidade de host: ");
+	scanf("%d",&host);
 	printf("Faixas de SubRede\n");
-	int c=0;
-	for(i=0;i<n;i++) {
+	
+	int cont=0;
+	for(i=0;i<qtda_subRede;i++) {
 		printf("Rede %d: ", i+1);
-		for(j=0;j<16;j++){
-			printf("%d ",c);
-			c++;
+		for(j=0;j<host;j++){
+			printf("%d ",cont);
+			cont++;
 		}
 		printf("\n");
 	}
