7aa6b3e6081d1f41d714c0ece7aedad9408d1e36
Alexsandro Cristiano <acg.alexsandro@hotmail.com>
2021-10-28 07:39:20 -0300

    Adiocina estrutura do menu
    - Adiocina a logica para menu
    - refatora nome de variavel tamanho para TAM
    - implementa a funcionalidade de verificar espaço no vetor
    - adiciona o setlocale

--- 3esd/vetor-dinamico/vetor-dinamico.c
+++ 3esd/vetor-dinamico/vetor-dinamico.c
@@ -1,72 +1,88 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <locale.h>
 
 #define true 1
 #define false 0
 
 typedef struct dados {
 	int numero;
-}TDados;
+} TDados;
 
 /*
 	Variaveis globais
 */
-int tamanho = 10;
+int TAM;
 
-/*
-CRIAR VETOR:
-		Parametro = tamanhoVetor 
-		Retorno vazio
-		- dupicar o tamanho original do vetor
-		- alocar memoria para um novo vetor
-		- retornar um vetor
-*/
-int criarVetor(int *tamanho);
-int adicionarElemento(TDados *dados, int tamanho, int elemento, int *qtdElemento);
+// prototipos das funções
+int criarVetor(int *TAM);
+int verificarEspacoLivre(int TAM, int qtdElemento);
+void adicionarElemento(TDados *dados, int elemento, int *qtdElemento);
 
 
-int main(){
+
+int main() {
+	setlocale(LC_ALL, "Portuguese");
+
+	
+	TAM = 2;
+	TDados dados[TAM];
+	
 	int qtdElemento = 0;
-	TDados dados[tamanho];
+	int opcao, elemento;
+
+	do {
+		printf("---MENU---\n1 - Adicionar\n2 - Inserir\n3 - Buscar Todos\n4 - Acessar\n5 - Deletar\n 0 - Encerrar\nEscolha: ");
+		scanf("%d",&opcao);
 	
-	int i,k=1;
-	
-	printf("Inicio do programa\n exibindo elementos do veotor\n");
-	for(i=0;i<tamanho; i++) {
-		printf("%d\n", dados[i].numero);
-	}
-	
-	
-	for(i=0;i<tamanho; i++) {
-		k*=2;
-		adicionarElemento(dados, tamanho,k, &qtdElemento);
-	}
-	printf("\nFim do for, elementos inseridos\n\n Apresentando elementos\n");
-	
-	for(i=0;i<tamanho; i++) {
-		printf("%d\n", dados[i].numero);
-	}
-	
+		switch(opcao) {
+			case 1:
+				printf("Informe um número: ");
+				scanf("%d", &elemento);
+
+				if(verificarEspacoLivre(TAM, qtdElemento)) {
+					adicionarElemento(dados,elemento, &qtdElemento);
+				}
+				else {
+					printf("QUANTIDADE DE ELEMENTO == TAM\n%d - %d", qtdElemento,TAM);
+				}
+				break;
+			case 2:
+				printf("Caso %d\n", opcao);
+				getchar();
+				break;
+			case 3:
+				
+				break;
+			case 4:
+				printf("Caso %d\n", opcao);
+				break;
+			case 5:
+				printf("Caso %d\n", opcao);
+				break;
+			case 0:
+				printf("Caso %d\n", opcao);
+				break;
+			default:
+				printf("Opção Inválida!\n");
+		}
+	} while(opcao!=0);
+
 	return 0;
 }
 
-int criarVetor(int *tamanho) {
-	return (*tamanho) *= 2;
+int criarVetor(int *TAM) {
+	return (*TAM) *= 2;
 //	char *matriz = (char*) malloc(strlen(vet) * sizeof(char));
-	
+
 }
 
-int adicionarElemento(TDados *dados, int tamanho, int elemento, int *qtdElemento) {
-	static int v=1;
-	int ind = *qtdElemento;
-	printf("\nFuncao adicionar elemento %d\n", v);
-	printf("---\n DADOS DE ENTRADA---\n TAM = %d\nELEM = %d\n QtdEleme = %d\n",tamanho, elemento,*qtdElemento );
+int verificarEspacoLivre(int TAM, int qtdElemento) {
+	return (qtdElemento < TAM)? true : false;
+}
 
-	dados[ind].numero = elemento;
-
+void adicionarElemento(TDados *dados, int elemento, int *qtdElemento) {
+	dados[*qtdElemento].numero = elemento;
 	(*qtdElemento)++;
-	v++;
-	return true;
 }
-
 
